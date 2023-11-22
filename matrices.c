386e7f037be42da48f05dba80237f17f0cdc3114
Martin Burga <martin.burga@udla.edu.ec>
2023-11-22 16:11:06 -0500

    Adicion de comentarios y ciertos arreglos en el codigo

--- matrices.c
+++ matrices.c
@@ -4,12 +4,15 @@ int main(void)
 {
     int di;
     int dj;
+    // Se crean las variables "di" "dj" que estas seran las principales encargadas de realizar la dimension de la matrices
+    // Segun el usuario desee. 
     printf("Inserta la dimension de la matriz en i\n");
     scanf("%d", &di);
     printf("Inserta la dimension de la matriz en j\n");
     scanf("%d", &dj);
     int matrix[di][dj];
-
+    // Apesar de que salga un error hay que ignorar dicho error que asoma aqui.
+    
     for (int i = 0; i < di; i++)
     {
         for (int j = 0; j < dj; j++)
@@ -21,6 +24,12 @@ int main(void)
             }
         }
     }
+
+    // Se va leyendo en orden de lectura de matrices. Con el primer for se lee las filas
+    // mientras que el segundo for lee las columnas. 
+    // EL if se encarga principalmente de escribir 1 en la diagonal princial acorde a que si
+    // le numero de filas coincide al numero de columnas 
+
     for (int i = 0; i < di; i++)
     {
         for (int j = 0; j < dj; j++)
@@ -29,5 +38,7 @@ int main(void)
         }
         printf("\n");
     }
+
+    // Finalmente se escribe la matriz con espacios de filas y entre columnas para una mas facil lectura.
     return 0;
 }
